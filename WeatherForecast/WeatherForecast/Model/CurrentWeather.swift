9fb6d67bf60363d1da9fb7e349ffdfb9d0eb05f9
Journey36 <be9.smile@gmail.com>
2021-08-24 14:59:01 +0900

    Refactor - Remove unnecessary constant

--- WeatherForecast/WeatherForecast/Model/CurrentWeather.swift
+++ WeatherForecast/WeatherForecast/Model/CurrentWeather.swift
@@ -9,13 +9,11 @@ import Foundation
 
 struct CurrentWeather: Decodable {
     let city: String
-    let coordinates: Coordinates
     let temperature: Temperature
     let weather: [Weather]
 
     enum CodingKeys: String, CodingKey {
         case city = "name"
-        case coordinates = "coord"
         case temperature = "main"
         case weather
     }
