316611e22dabf22d0a24d6ab6b93615e807530a4
Journey36 <be9.smile@gmail.com>
2021-09-09 11:23:52 +0900

    Feature: typealias 생성

---
+++ WeatherForecast/WeatherForecast/Types/CurrentLocation.swift
@@ -0,0 +1,10 @@
+//
+//  CurrentLocation.swift
+//  WeatherForecast
+//
+//  Created by Journey36 on 2021/09/09.
+//
+
+import Foundation
+
+typealias CurrentLocation = (latitude: String, longitude: String)
