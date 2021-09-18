94e2f39d7cd6d6b4d9b83af7484d7d8e2d0bd89f
Journey36 <be9.smile@gmail.com>
2021-09-19 04:39:29 +0900

    Feature: 테이블 뷰 생성
    <Descriptions>
    + Custom Cell 등록
    + separatorInset 조절 및 셀 선택 불가능하게 설정

---
+++ WeatherForecast/WeatherForecast/View/ForecastListView.swift
@@ -0,0 +1,35 @@
+//
+//  ForecastListView.swift
+//  WeatherForecast
+//
+//  Created by Journey36 on 2021/09/16.
+//
+
+import UIKit
+
+final class ForecastListView: UITableView {
+    // MARK:- Methods
+    // MARK: Custom
+    private func registerCells() {
+        register(ForecastListCell.self, forCellReuseIdentifier: ForecastListCell.identifier)
+        register(CurrentWeatherCell.self, forCellReuseIdentifier: CurrentWeatherCell.identifier)
+    }
+
+    private func configureAttributes() {
+        separatorInset = TableView.separateInset
+        allowsSelection = false
+    }
+    
+    // MARK:- Initializers
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        configureAttributes()
+        registerCells()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureAttributes()
+        registerCells()
+    }
+}
