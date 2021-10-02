027bc7163a57a396e252f1ea28b54454332c8f63
Journey36 <be9.smile@gmail.com>
2021-10-03 01:39:35 +0900

    Feature: 이미지 로드 태스크 구현

--- WeatherForecast/WeatherForecast/Contoller/NetworkController/DataTaskManager.swift
+++ WeatherForecast/WeatherForecast/Contoller/NetworkController/DataTaskManager.swift
@@ -5,7 +5,7 @@
 //  Created by Journey36 on 2021/09/07.
 //
 
-import Foundation
+import UIKit
 
 struct DataTaskManager {
     let session: URLSession = .shared
@@ -30,12 +30,36 @@ struct DataTaskManager {
             
             do {
                 let decoder: JSONDecoder = .init()
+                // FIXME: 네이밍 변경 요망
                 let currentWeatherData: T = try decoder.decode(T.self, from: data)
                 return completion(.success(currentWeatherData))
             } catch {
                 completion(.failure(.SystemError(type: .invalidData)))
             }
             
+        }.resume()
+    }
+
+    func fetchWeatherIcon(_ imageID: String, completion: @escaping (Result<UIImage, ErrorHandler>) -> Void) {
+        guard let targetURL: URL = try? URLManager.setImageURL(of: imageID) else {
+            // TODO: 에러 핸들링
+            return
+        }
+
+        session.dataTask(with: targetURL) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let _: Error = error {
+                return
+            }
+
+            guard let response: HTTPURLResponse = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                return
+            }
+
+            guard let data: Data = data, let iconImage: UIImage = .init(data: data) else {
+                return
+            }
+            
+            return completion(.success(iconImage))
         }.resume()
     }
 }
