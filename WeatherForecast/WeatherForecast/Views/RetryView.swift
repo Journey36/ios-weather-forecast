6af984272003e6e99c766c73bd2f920a6fe6d022
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-10-08 03:56:21 +0900

    [#42] contentColor 변경

--- WeatherForecast/WeatherForecast/Views/RetryView.swift
+++ WeatherForecast/WeatherForecast/Views/RetryView.swift
@@ -8,21 +8,7 @@
 import UIKit
 
 class RetryView: UIView {
-    private static let contentColor = UIColor.gray
-//    private let contentFont = UIFont.TextStyle.body
-    
-    let retryButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.titleLabel?.adjustsFontForContentSizeCategory = true
-        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .subheadline)
-        button.setTitle("재시도", for: .normal)
-        button.setTitleColor(contentColor, for: .normal)
-        button.layer.borderWidth = 1
-        button.layer.borderColor = contentColor.cgColor
-        button.contentEdgeInsets = UIEdgeInsets(top: 8, left: 50, bottom: 8, right: 50)
-        return button
-    }()
+    private static let contentColor = UIColor.init(displayP3Red: 99/255, green: 99/255, blue: 102/255, alpha: 1.0)
     
     private let messageLabel: UILabel = {
         let label = UILabel()
@@ -33,6 +19,20 @@ class RetryView: UIView {
         label.textAlignment = .center
         label.numberOfLines = 0
         return label
+    }()
+    
+    let retryButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.adjustsFontForContentSizeCategory = true
+        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        button.setTitle("재시도", for: .normal)
+        button.setTitleColor(contentColor, for: .normal)
+        button.layer.borderWidth = 1
+        button.layer.borderColor = contentColor.cgColor
+        button.layer.cornerRadius = 5
+        button.contentEdgeInsets = UIEdgeInsets(top: 8, left: 50, bottom: 8, right: 50)
+        return button
     }()
     
     override init(frame: CGRect) {
@@ -51,21 +51,22 @@ class RetryView: UIView {
     }
     
     private func configureSubviews() {
-        configureRetryButton()
-        configureMessageLabel()
+        addSubviews()
+        configureAutoLayout()
     }
     
-    private func configureRetryButton() {
+    private func addSubviews() {
         addSubview(retryButton)
-        retryButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        retryButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        addSubview(messageLabel)
     }
     
-    private func configureMessageLabel() {
-        addSubview(messageLabel)
+    private func configureAutoLayout() {
         messageLabel.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor).isActive = true
         messageLabel.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor).isActive = true
         messageLabel.topAnchor.constraint(greaterThanOrEqualTo: layoutMarginsGuide.topAnchor).isActive = true
         messageLabel.bottomAnchor.constraint(equalTo: retryButton.topAnchor, constant: -30).isActive = true
+        
+        retryButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        retryButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
     }
 }
