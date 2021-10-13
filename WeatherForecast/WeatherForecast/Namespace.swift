f677cc362f80746da175b577eed9ef5e00fba836
Journey36 <be9.smile@gmail.com>
2021-10-13 18:59:41 +0900

    :coffin: 불필요한 코드 삭제

--- WeatherForecast/WeatherForecast/Namespace.swift
+++ WeatherForecast/WeatherForecast/Namespace.swift
@@ -7,15 +7,6 @@
 
 import UIKit
 
-enum TableView {
-    static let separateInset: UIEdgeInsets = .init(top: 0, left: 10, bottom: 0, right: 10)
-    static let numberOfSections: Int = 2
-    
-    static func estimateRowHeight(of section: Int) -> CGFloat {
-        return section == 0 ? 60 : 30
-    }
-}
-
 enum Units {
     static let temperatureUnit: String = "\u{00B0}"
 }
